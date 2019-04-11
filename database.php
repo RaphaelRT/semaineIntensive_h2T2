1edf48c7e5c7d0c5341237f0205ffa48336b0cf2
RaphaelRT <raph.robert974@gmail.com>
2019-04-11 11:13:41 +0200

    fix

--- database.php
+++ database.php
@@ -1,24 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-define('DB_HOST', 'localhost');
-define('DB_PORT', '3308');
-define('DB_NAME', 'poop_advisor');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-
-try {
-    $pdo = new PDO(
-        'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';port=' . DB_PORT,
-        DB_USER,
-        DB_PASS
-    );
-
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-} catch (PDOException $e) {
-    die('Cannot connect');
-}
-=======
     define('DB_HOST', 'localhost');
     define('DB_PORT', '3308');
     define('DB_NAME', 'poop_advisor');
@@ -39,4 +20,3 @@ try {
     {
         die('Cannot connect');
 }
->>>>>>> 92e8adf3a95f3d9e10fb66f394573cb100c55f68
