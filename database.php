14d752f6fc8742e1da889801c9dd6d3e47b457b9
PJoessel <pjoessel@gmail.com>
2019-04-11 11:06:17 +0200

    database

--- database.php
+++ database.php
@@ -4,19 +4,16 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', '3308');
 define('DB_NAME', 'poop_advisor');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'root');
 
-try
-{
+try {
     $pdo = new PDO(
-        'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT,
+        'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';port=' . DB_PORT,
         DB_USER,
         DB_PASS
     );
 
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-}
-catch(PDOException $e)
-{
+} catch (PDOException $e) {
     die('Cannot connect');
 }
